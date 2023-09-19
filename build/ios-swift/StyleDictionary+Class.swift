ed8ec26d60a545b587c8c3d9d8d7c6e99373dedc
m-milde <m.milde@tinqin.com>
2023-09-19 14:09:46 +0300

    red to blue

--- build/ios-swift/StyleDictionary+Class.swift
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -4,12 +4,12 @@
 //
 
 // Do not edit directly
-// Generated on Tue, 19 Sep 2023 11:04:47 GMT
+// Generated on Tue, 19 Sep 2023 11:08:56 GMT
 
 
 import UIKit
 
 public class StyleDictionaryClass {
     public static let brandNewSize = 20
-    public static let globalBase = #e60000
+    public static let globalBase = #0010e6
 }
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,12 +4,12 @@
 //
 
 // Do not edit directly
-// Generated on Tue, 19 Sep 2023 11:04:47 GMT
+// Generated on Tue, 19 Sep 2023 11:08:56 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
     internal static let brandNewSize = 20
-    internal static let globalBase = #e60000
+    internal static let globalBase = #0010e6
 }
