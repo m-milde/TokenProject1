a8254547a1a64fe98848540cbb122deec7cb35d5
m-milde <m.milde@tinqin.com>
2023-09-19 15:44:36 +0300

    new build

--- build/ios-swift/StyleDictionary+Class.swift
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -4,12 +4,49 @@
 //
 
 // Do not edit directly
-// Generated on Tue, 19 Sep 2023 12:43:19 GMT
+// Generated on Tue, 19 Sep 2023 12:44:12 GMT
 
 
 import UIKit
 
 public class StyleDictionaryClass {
-    public static let brandNewSize = 20
-    public static let globalBase = #0010e6
+    public static let colorsRootMode1BrandBrand = #f9b11eff
+    public static let colorsRootMode1BrandBrandDark = #f39100ff
+    public static let colorsRootMode1BrandBrandLight = #fdcd6eff
+    public static let colorsRootMode1BrandBrandLighter = #ffefaeff
+    public static let colorsRootMode1BrandBrandNoir10 = #ececedff
+    public static let colorsRootMode1BrandBrandNoir40 = #b1b2b3ff
+    public static let colorsRootMode1BrandBrandNoir5 = #f6f6f6ff
+    public static let colorsRootMode1BrandBrandNoir60 = #868786ff
+    public static let colorsRootMode1ErrorError = #e84362ff
+    public static let colorsRootMode1ErrorErrorDark = #c7305bff
+    public static let colorsRootMode1ErrorErrorDarker = #a72154ff
+    public static let colorsRootMode1ErrorErrorLight = #f1707cff
+    public static let colorsRootMode1ErrorErrorLighter = #fde0d9ff
+    public static let colorsRootMode1InfoInfo = #25aebfff
+    public static let colorsRootMode1InfoInfoDark = #1b8aa4ff
+    public static let colorsRootMode1InfoInfoDarker = #126989ff
+    public static let colorsRootMode1InfoInfoLight = #1ecce1ff
+    public static let colorsRootMode1InfoInfoLighter = #d3fbf3ff
+    public static let colorsRootMode1NeutralNeutralBlank = #ffffffff
+    public static let colorsRootMode1NeutralNeutralGrey1 = #fdfdfdff
+    public static let colorsRootMode1NeutralNeutralGrey2 = #fafafaff
+    public static let colorsRootMode1NeutralNeutralGrey3 = #f1f1f1ff
+    public static let colorsRootMode1NeutralNeutralGrey4 = #d2d2d6ff
+    public static let colorsRootMode1NeutralNeutralGrey5 = #b4b3bcff
+    public static let colorsRootMode1NeutralNeutralGrey6 = #a7a6b1ff
+    public static let colorsRootMode1NeutralNeutralGrey7 = #6f6f79ff
+    public static let colorsRootMode1NeutralNeutralGrey8 = #50505eff
+    public static let colorsRootMode1NeutralNeutralGrey9 = #302f3cff
+    public static let colorsRootMode1NeutralNeutralNoir = #000000ff
+    public static let colorsRootMode1SuccessSuccess = #099735ff
+    public static let colorsRootMode1SuccessSuccessDark = #068138ff
+    public static let colorsRootMode1SuccessSuccessDarker = #046c39ff
+    public static let colorsRootMode1SuccessSuccessLight = #38c055ff
+    public static let colorsRootMode1SuccessSuccessLighter = #cef9caff
+    public static let colorsRootMode1WarningWarning = #ffcd1aff
+    public static let colorsRootMode1WarningWarningDark = #e09f1bff
+    public static let colorsRootMode1WarningWarningDarker = #c78e18ff
+    public static let colorsRootMode1WarningWarningLight = #ffdd53ff
+    public static let colorsRootMode1WarningWarningLighter = #fff9d1ff
 }
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,12 +4,49 @@
 //
 
 // Do not edit directly
-// Generated on Tue, 19 Sep 2023 12:43:19 GMT
+// Generated on Tue, 19 Sep 2023 12:44:12 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
-    internal static let brandNewSize = 20
-    internal static let globalBase = #0010e6
+    internal static let colorsRootMode1BrandBrand = #f9b11eff
+    internal static let colorsRootMode1BrandBrandDark = #f39100ff
+    internal static let colorsRootMode1BrandBrandLight = #fdcd6eff
+    internal static let colorsRootMode1BrandBrandLighter = #ffefaeff
+    internal static let colorsRootMode1BrandBrandNoir10 = #ececedff
+    internal static let colorsRootMode1BrandBrandNoir40 = #b1b2b3ff
+    internal static let colorsRootMode1BrandBrandNoir5 = #f6f6f6ff
+    internal static let colorsRootMode1BrandBrandNoir60 = #868786ff
+    internal static let colorsRootMode1ErrorError = #e84362ff
+    internal static let colorsRootMode1ErrorErrorDark = #c7305bff
+    internal static let colorsRootMode1ErrorErrorDarker = #a72154ff
+    internal static let colorsRootMode1ErrorErrorLight = #f1707cff
+    internal static let colorsRootMode1ErrorErrorLighter = #fde0d9ff
+    internal static let colorsRootMode1InfoInfo = #25aebfff
+    internal static let colorsRootMode1InfoInfoDark = #1b8aa4ff
+    internal static let colorsRootMode1InfoInfoDarker = #126989ff
+    internal static let colorsRootMode1InfoInfoLight = #1ecce1ff
+    internal static let colorsRootMode1InfoInfoLighter = #d3fbf3ff
+    internal static let colorsRootMode1NeutralNeutralBlank = #ffffffff
+    internal static let colorsRootMode1NeutralNeutralGrey1 = #fdfdfdff
+    internal static let colorsRootMode1NeutralNeutralGrey2 = #fafafaff
+    internal static let colorsRootMode1NeutralNeutralGrey3 = #f1f1f1ff
+    internal static let colorsRootMode1NeutralNeutralGrey4 = #d2d2d6ff
+    internal static let colorsRootMode1NeutralNeutralGrey5 = #b4b3bcff
+    internal static let colorsRootMode1NeutralNeutralGrey6 = #a7a6b1ff
+    internal static let colorsRootMode1NeutralNeutralGrey7 = #6f6f79ff
+    internal static let colorsRootMode1NeutralNeutralGrey8 = #50505eff
+    internal static let colorsRootMode1NeutralNeutralGrey9 = #302f3cff
+    internal static let colorsRootMode1NeutralNeutralNoir = #000000ff
+    internal static let colorsRootMode1SuccessSuccess = #099735ff
+    internal static let colorsRootMode1SuccessSuccessDark = #068138ff
+    internal static let colorsRootMode1SuccessSuccessDarker = #046c39ff
+    internal static let colorsRootMode1SuccessSuccessLight = #38c055ff
+    internal static let colorsRootMode1SuccessSuccessLighter = #cef9caff
+    internal static let colorsRootMode1WarningWarning = #ffcd1aff
+    internal static let colorsRootMode1WarningWarningDark = #e09f1bff
+    internal static let colorsRootMode1WarningWarningDarker = #c78e18ff
+    internal static let colorsRootMode1WarningWarningLight = #ffdd53ff
+    internal static let colorsRootMode1WarningWarningLighter = #fff9d1ff
 }
