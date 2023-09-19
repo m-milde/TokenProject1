fec3756ac7eea815eb3f2ee7a2e2e095e5e0b110
m-milde <m.milde@tinqin.com>
2023-09-19 22:50:24 +0300

    new js and css

---
+++ build/js/variables.js
@@ -0,0 +1,7 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 19 Sep 2023 19:49:59 GMT
+ */
+
+export const ColorIbaBrand = "#f9b11e";
+export const DsDropShadow = [{"color":"#00000033","type":"dropShadow","x":0,"y":1,"blur":2,"spread":0},{"color":"#00000033","type":"dropShadow","x":0,"y":7,"blur":20,"spread":0}];
