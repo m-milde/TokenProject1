122ba36de8e9ce293ec0e1a517e234b5f73a3a8c
m-milde <m.milde@tinqin.com>
2023-09-19 23:24:23 +0300

    add ds

--- build/js/variables.js
+++ build/js/variables.js
@@ -1,7 +1,7 @@
 /**
  * Do not edit directly
- * Generated on Tue, 19 Sep 2023 19:49:59 GMT
+ * Generated on Tue, 19 Sep 2023 20:22:33 GMT
  */
 
 export const ColorIbaBrand = "#f9b11e";
-export const DsDropShadow = [{"color":"#00000033","type":"dropShadow","x":0,"y":1,"blur":2,"spread":0},{"color":"#00000033","type":"dropShadow","x":0,"y":7,"blur":20,"spread":0}];
+export const DsDropShadow = [{"color":"rgba(0,0,0,0.4)","type":"dropShadow","x":0,"y":1,"blur":2,"spread":0},{"color":"rgba(0,0,0,0.4)","type":"dropShadow","x":0,"y":7,"blur":20,"spread":0}];
